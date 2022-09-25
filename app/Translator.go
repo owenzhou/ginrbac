1a72fefec95b4a778e5e7c2c804eb9fb660537f7
owenzhou <416789207@qq.com>
2022-09-25 20:30:28 +0800

    优化

--- app/Translator.go
+++ app/Translator.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"strconv"
+
 	"github.com/owenzhou/ginrbac/support/facades"
 
 	"github.com/go-playground/validator/v10"
@@ -9,9 +11,16 @@ import (
 //将validator错误信息翻译成中文
 func Translate(err error) []string {
 	var result = make([]string, 0)
-	errors := err.(validator.ValidationErrors)
-	for _, err := range errors {
-		result = append(result, err.Translate(facades.Translator))
+	switch err.(type) {
+	case *strconv.NumError:
+		errors := err.Error()
+		result = append(result, errors)
+		return result
+	default:
+		errors := err.(validator.ValidationErrors)
+		for _, err := range errors {
+			result = append(result, err.Translate(facades.Translator))
+		}
+		return result
 	}
-	return result
 }
